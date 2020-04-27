5d31179ba31e558dfa7d5eb3f9b7ace65bb57cbc
Nicholas <32116122+nicholasmy@users.noreply.github.com>
2020-04-27 15:00:05 -0400

    Add !cal, enumerate !show, reformat !help

--- bot.py
+++ bot.py
@@ -4,38 +4,39 @@ import sys
 from discord.ext import commands
 from collections import defaultdict
 
+
 def isTA(usr: discord.Member):
     roles = usr.roles
     for x in roles:
-        if x.name == "TA":
+        if x.name.upper() == "TA":
             return True
     return False
 
+
 def printQ(q):
     st = "Queue is:\n"
-    for ele in q:
-        st += ele.name
-        st += '\n'
+    for x, ele in enumerate(q):
+        st += "{}. {}\n".format(x + 1, ele.name)
     return st
 
+
 client = discord.Client()
-id_to_list = {} #string->List dictionary
+id_to_list = {}  # string->List dictionary
 
 
 @client.event
-async def on_ready(): #onready is called after all guilds are added to client
+async def on_ready():  # onready is called after all guilds are added to client
     print('We have logged in as {0.user}'.format(client))
-
 
 
 @client.event
 async def on_message(message):
-    global id_to_list            
+    global id_to_list
     thisid = str(message.guild.id)
     if message.author == client.user:
-        return # prevents infinite loops of reading own message
+        return  # prevents infinite loops of reading own message
 
-    if message.channel.name == "office-hours-queue": # only want messages in OH
+    if message.channel.name == "office-hours-queue":  # only want messages in OH
 
         #               enqueue
         if message.content.startswith('!enqueue') or message.content.startswith('!E'):
@@ -49,11 +50,11 @@ async def on_message(message):
                     msg = "{} you were already in the list! You have been moved to the back.".format(name)
                     await message.channel.send(msg)
                 else:
-                    if(len(queue) == 0):
+                    if (len(queue) == 0):
                         msg = "{} you have been succesfully added to the queue, and you are first in line!".format(name)
                         queue.append(stu)
                         await message.channel.send(msg)
-                    else:   
+                    else:
                         queue.append(stu)
                         msg = "{} you have been succesfully added to the queue in position: {}".format(name, len(queue))
                         await message.channel.send(msg)
@@ -63,7 +64,6 @@ async def on_message(message):
                 msg = "{} you have been succesfully added to the queue, and you are next!".format(name)
                 await message.channel.send(msg)
 
-            
         #               leave queue
         if message.content.startswith('!leave') or message.content.startswith('!L'):
             stu = message.author
@@ -78,56 +78,59 @@ async def on_message(message):
                     msg = "{}, according to my records, you were already not in the queue.".format(name)
                     await message.channel.send(msg)
             else:
-                #leave called before any enqueues
+                # leave called before any enqueues
                 print("edge case")
                 msg = "{}, according to my records, you were already not in the queue.".format(name)
                 await message.channel.send(msg)
 
+        if message.content.startswith('!cal'):
+            msg = "Here's the TA Schedule on Google Calendar.\nhttps://tinyurl.com/116ohCalender"
+            await message.channel.send(msg)
 
         #               dequeue: TA only
         if (message.content.startswith('!dequeue') or message.content.startswith('!D')) and isTA(message.author):
             ta = message.author.mention
             if thisid in id_to_list:
                 queue = id_to_list[thisid]
-                if(len(queue) > 0):
+                if (len(queue) > 0):
                     stu = queue.pop(0)
                     msg = "{}, you are next! {} is available to help you now!".format(stu.mention, ta)
                     await message.channel.send(msg)
                 else:
-                    #no one in queue
+                    # no one in queue
                     msg = "Good job TAs! The Queue is empty!"
                     await message.channel.send(msg)
             else:
-                #called before anyone enqueued
+                # called before anyone enqueued
                 msg = "Good job TAs! The Queue is empty!"
                 await message.channel.send(msg)
-
 
         #           Clear queue: TA only
         if (message.content.startswith('!clearqueue') or message.content.startswith('!C')) and isTA(message.author):
             id_to_list[thisid] = []
-            msg = "Cleared the list."
+            msg = "Cleared the queue."
             await message.channel.send(msg)
 
         #              show queue
-        if message.content.startswith('!show') or message.content.startswith('!showqueue') or message.content.startswith('!S'):
+        if message.content.startswith('!show') or message.content.startswith(
+                '!showqueue') or message.content.startswith('!S'):
             if thisid in id_to_list:
                 queue = id_to_list[thisid]
                 if queue:
-                    #printQ
+                    # printQ
                     msg = printQ(queue)
                     await message.channel.send(msg)
                 else:
                     msg = "The queue is empty right now."
-                    await message.channel.send(msg)    
+                    await message.channel.send(msg)
             else:
                 msg = "The queue is empty right now."
                 id_to_list[thisid] = []
                 await message.channel.send(msg)
 
-        #help
+        # help
         if message.content.startswith('!help'):
-            msg = ''' To enqueue yourself, send a "!enqueue" or "!E" message. To see the current Queue, send a "!show" or "!S" message. TA's will dequeue you with a "!dequeue" or "!D" message. To leave the queue, you can use "!leave" or "!L".'''
+            msg = '''To enqueue yourself, send a "!enqueue" or "!E" message.\nTo see the current Queue, send a "!show" or "!S" message.\nTA's will dequeue you with a "!dequeue" or "!D" message.\nTo leave the queue, you can use "!leave" or "!L".\nYou can view the office hours schedule with "!cal". '''
             await message.channel.send(msg)
 
     # TODO:
@@ -143,4 +146,3 @@ if __name__ == "__main__":
     mytoken = sys.argv[1]
     client.run(mytoken)  # TODO: system env to run from a bat script to keep my token safe online
     main()
-
